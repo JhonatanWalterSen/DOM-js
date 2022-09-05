ebdc1db0855dbea7eb90cf97b96e612b42396cf9
Jhonatan Ariza <arizaj@LIM915010.local>
2022-09-05 12:05:04 -0500

    ajustes dom

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,11 @@
 import scrollTopButtom from "./dom/boton_scroll.js";
 import countdown from "./dom/cuentaRegresiva.js";
+import userDeviceInfo from "./dom/deteccion_dispositivos.js";
+import networkStatus from "./dom/deteccion_red.js";
+import webCam from "./dom/deteccion_webcam.js";
 import hamburguerMenu from "./dom/menu-hamburguesa.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
+import responsiveTester from "./dom/prueba_responsive.js";
 import { digitalClock, alarma } from "./dom/reloj.js";
 import { moveBall, shortCut } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
@@ -13,6 +18,11 @@ d.addEventListener('DOMContentLoaded', e=>{
     alarma('assets/Alarma-kikiriki.mp3','#activar-alarma','#desactivar-alarma')
     countdown('countdown','Jun 7, 2023','Feliz Cumpleaños')
     scrollTopButtom('.scroll-top-btn')
+    responsiveMedia('youtube','(min-width: 1024px)','<a href="https://youtu.be/y3kaWI1cycg">Ver Video</a>','<iframe width="560" height="315" src="https://www.youtube.com/embed/y3kaWI1cycg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    responsiveMedia('gmaps','(min-width: 1024px)','Contenido Movil','Contenido Escritorio')
+    responsiveTester('responsive-tester')
+    userDeviceInfo('user-device')
+    webCam('webcam')
 })
 
 
@@ -21,3 +31,4 @@ d.addEventListener('keydown', e =>{
     moveBall(e,'.ball','.stage')
 })
 darkTheme('.dark-theme-btn','dark-mode')
+networkStatus()
